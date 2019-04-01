400aaca7d96d0b36c3b8704d18d96f045ef28f17
Aleksandr Busygin <persik665@yandex.ru>
2019-04-02 01:31:19 +0300

    2

--- src/main/java/steps/ShoppingSteps.java
+++ src/main/java/steps/ShoppingSteps.java
@@ -3,6 +3,7 @@ package steps;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import pages.ShoppingPage;
 import util.Init;
@@ -13,36 +14,31 @@ public class ShoppingSteps {
 
     @When("в корзину добавлен первый продукт с наименованием \"(.+)\"")
     public void stepAddFirstProduct(String productName){
-        JavascriptExecutor jse = (JavascriptExecutor) Init.getDriver();
-        jse.executeScript("window.scrollBy(0,400)", "");
+        shoppingPage.scrollToElement(shoppingPage.getProduct(productName));
         shoppingPage.addProduct(productName);
     }
 
     @When("в корзину добавлен второй продукт с наименованием \"(.+)\"")
     public void stepAddSecondProduct(String productName){
-        JavascriptExecutor jse = (JavascriptExecutor) Init.getDriver();
-        jse.executeScript("window.scrollBy(0,400)", "");
+        shoppingPage.scrollToElement(shoppingPage.getProduct(productName));
         shoppingPage.addProduct(productName);
     }
 
     @When("в корзину добавлен третий продукт с наименованием \"(.+)\"")
     public void stepAddThirdProduct(String productName){
-        JavascriptExecutor jse = (JavascriptExecutor) Init.getDriver();
-        jse.executeScript("window.scrollBy(0,400)", "");
+        shoppingPage.scrollToElement(shoppingPage.getProduct(productName));
         shoppingPage.addProduct(productName);
     }
 
     @When("в корзину добавлен четвертый продукт с наименованием \"(.+)\"")
     public void stepAddFourthProduct(String productName){
-        JavascriptExecutor jse = (JavascriptExecutor) Init.getDriver();
-        jse.executeScript("window.scrollBy(0,400)", "");
+        shoppingPage.scrollToElement(shoppingPage.getProduct(productName));
         shoppingPage.addProduct(productName);
     }
 
     @When("в корзину добавлен пятый продукт с наименованием \"(.+)\"")
     public void stepAddFifthProduct(String productName){
-        JavascriptExecutor jse = (JavascriptExecutor) Init.getDriver();
-        jse.executeScript("window.scrollBy(0,400)", "");
+        shoppingPage.scrollToElement(shoppingPage.getProduct(productName));
         shoppingPage.addProduct(productName);
     }
 
